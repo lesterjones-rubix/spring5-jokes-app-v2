277d530c4943e5733d3d95cc52e702c9ac9a4a3b
Lester Jones <lester.jones@rubix.nl>
2021-02-16 09:37:19 +0100

    Added Service Interface and Implementation to return a random chuck norris quote closes #3

---
+++ src/main/java/guru/springframework/spring5jokesappv2/services/JokeServiceImpl.java
@@ -0,0 +1,22 @@
+package guru.springframework.spring5jokesappv2.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    // This is not friendly for dependency injection
+    // If were are a little further in utilizing the spring configuration
+    // That would declare this as a bean.
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
